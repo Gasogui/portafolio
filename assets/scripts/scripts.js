893ea0cad20ab6e2c43f815fca40598e2a4e1e2d
Guillermo Garcia Soto <gasogui@gmail.com>
2021-08-06 22:50:33 -0500

    Maquetado final

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -11,3 +11,40 @@ navToggle.addEventListener("click", () => {
         navToggle.setAttribute("aria-label", "Abrir menú");
     }
 });
+
+
+/* *************** ContactForm ************** */
+((d) => {
+    const $form = d.querySelector(".contact-form"),
+        $loader = d.querySelector(".contact-form-loader"),
+        $response = d.querySelector(".contact-form-response");
+
+    $form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        $loader.classList.remove("none");
+        fetch("https://formsubmit.co/ajax/gasogui@gmail.com", {
+            method: "POST",
+            body: new FormData(e.target),
+        })
+            .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+            .then((json) => {
+                console.log(json);
+                location.hash = "#gracias";
+                $form.reset();
+            })
+            .catch((err) => {
+                console.log(err);
+                let message =
+                    err.statusText || "Ocurrió un error al enviar, intenta nuevamente";
+                $response.querySelector(
+                    "h3"
+                ).innerHTML = `Error ${err.status}: ${message}`;
+            })
+            .finally(() => {
+                $loader.classList.add("none");
+                setTimeout(() => {
+                    location.hash = "#close";
+                }, 3000);
+            });
+    });
+})(document);
